bb44df6b51b4abb72761848c2789f943dcd52849
alanmontgomery <alan.montgomery@tascomi.com>
2021-07-23 15:15:33 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-pinterest-floating-tab-bar',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
